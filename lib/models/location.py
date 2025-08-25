c68d4d4ff3fb1e59f1104c11b6f2fa749dd571f1
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-26 00:58:31 +0300

    Changes properties to @validates

--- lib/models/location.py
+++ lib/models/location.py
@@ -1,6 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from __init__ import Base
+from sqlalchemy.orm import sessionmaker, validates
 
+Base = declarative_base()
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class Location(Base):
     __tablename__ = "locations"
@@ -11,55 +17,35 @@ class Location(Base):
     num_bars = Column(Integer, default=0)
     num_toilets = Column(Integer, default=0)
 
-    def __init__(self, name, capacity, num_bars, num_toilets, id=None):
-        self.id = id
-        self.name = name
-        self.capacity = capacity
-        self.num_bars = num_bars
-        self.num_toilets = num_toilets
+    # def __init__(self, name, capacity, num_bars, num_toilets, id=None):
+    #     self.id = id
+    #     self.name = name
+    #     self.capacity = capacity
+    #     self.num_bars = num_bars
+    #     self.num_toilets = num_toilets
 
     def __repr__(self):
         return f"<Location {self.name}, capacity={self.capacity}, Bars={self.num_bars}, Toilets={self.num_toilets}>"
+    
 
-    @property
-    def num_bars(self):
-        return self._num_bars
+    @validates("num_bars", "num_toilets")
+    def validate_numbers(self, key, value):
+        if not isinstance(value, int) or value < 0:
+            raise ValueError(f"{key} must be an integer")
+        return value
 
-    @num_bars.setter
-    def numbars(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Number of bars must be an integer")
-        self.num_bars = value
 
-    @property
-    def num_toilets(self):
-        return self._num_toilets
+    @validates("capacity")
+    def validate_capacity(self, key, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Capacity must be a positive integer")
+        return value
 
-    @num_toilets.setter
-    def num_toilets(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Number of toilets must be an integer")
-        self.num_toilets = value
-
-    @property
-    def capacity(self):
-        return self._capacity
-
-    @capacity.setter
-    def capacity(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Capacity must be a number")
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        if isinstance(name, str) and len(name):
-            return self._name = name
-        else:
-            raise ValueError("Please enter a location")
+    @validates
+    def validate_name(self, ket, value):
+        if not isinstance(value, str) or not value.strip():
+            raise ValueError("Please enter a location name")
+        return value
 
     @classmethod
     def create(cls, session, name, capacity, num_bars=0, num_toilets=0):
@@ -82,8 +68,13 @@ class Location(Base):
     def find_by_id(cls, session, location_id):
         return session.get(cls, location_id)
 
-    def delete(self):
+    def delete(self, session):
         session.delete(self)
         session.commit()
+
+    # creates the location table
+Base.metadata.create_all(engine) 
+
+
 
     
