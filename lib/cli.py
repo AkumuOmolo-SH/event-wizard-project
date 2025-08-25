c53e035d944a943fc74a1433dd6c6ddda80776c1
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-26 00:45:51 +0300

    Adds database for testing

---
+++ lib/cli.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Location
+
+
+engine = create_engine("sqlite:///:memory:", echo=True)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
