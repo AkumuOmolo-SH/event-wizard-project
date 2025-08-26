2d408fcc7f36350e5cc7c432f6f29adfc08b8a14
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-26 17:24:03 +0300

    Adds seeded database

--- lib/seed.py
+++ lib/seed.py
@@ -1,19 +1,40 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.location import Location, Base
+#!/usr/bin/env python3
+from models.attendee import Attendee
+from models.event import Event
+from models.location import Location
+from models.__init__ import CONN, CURSOR
+import sqlite3
 
-engine = create_engine("sqlite:///mydb.db", echo=True)
-Base.metadata.create_all(engine)
+conn = sqlite3.connect("events_wiz.db")
+cursor = conn.cursor()
 
-Session = sessionmaker(bind=engine)
-session = Session()
 
-loc1 = Location.create(session, name="Alchemist", capacity=500, num_bars=2, num_toilets=3)
-loc2 = Location.create(session, name="Muse", capacity=150, num_bars=5, num_toilets=4)
-loc3 = Location.create(session, name="Geco", capacity=250, num_bars=2, num_toilets=3)
+def seed_database():
+    Attendee.drop_table()
+    Event.drop_table()
+    Location.drop_table()
 
-print("Seeded locations:")
-for loc in Location.get_all(session):
-    print(loc)
+    Location.create_table()
+    Event.create_table()
+    Attendee.create_table()
 
-session.close()
+    alchemist = Location.create("Alchemist", 500, 5, 3)
+    geco = Location.create("Geco", 350, 4, 4)
+    living_rooms = Location.create("Living Rooms", 200, 2, 1)
+
+    concert = Event.create("AfroBeats Festival", "20", alchemist.id)
+    match = Event.create("Championship Final", "35", living_rooms.id)
+    dance = Event.create("Salsa class", "55", geco.id)
+
+    Attendee.create("M", concert.id, 2000.0)
+    Attendee.create("F", concert.id, 2000.0)
+    Attendee.create("M", match.id, 1500.0)
+    Attendee.create("Other", dance.id, 1000.0)
+    Attendee.create("F", concert.id, 2000.0)
+    Attendee.create("M", concert.id, 2000.0)
+    Attendee.create("M", match.id, 1500.0)
+    Attendee.create("Other", dance.id, 1000.0)
+
+
+seed_database()
+print("Seeded database")
