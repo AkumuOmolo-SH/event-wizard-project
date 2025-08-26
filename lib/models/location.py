2f86cda6c5f3bd371c6ef25c1ce8227dddd553bb
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-26 11:49:06 +0300

    Adds methods to Location class

--- lib/models/location.py
+++ lib/models/location.py
@@ -1,80 +1,152 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from __init__ import Base
-from sqlalchemy.orm import sessionmaker, validates
+import sqlite3
+from models.__init__ import CURSOR, CONN
 
-Base = declarative_base()
+conn = sqlite3.connect(":memory:")
+cursor = conn.cursor()
 
-engine = create_engine('sqlite:///:memory:', echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
 
-class Location(Base):
-    __tablename__ = "locations"
+class Location:
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
-    capacity = Column(Integer, nullable=False)
-    num_bars = Column(Integer, default=0)
-    num_toilets = Column(Integer, default=0)
+    all = {}
 
-    # def __init__(self, name, capacity, num_bars, num_toilets, id=None):
-    #     self.id = id
-    #     self.name = name
-    #     self.capacity = capacity
-    #     self.num_bars = num_bars
-    #     self.num_toilets = num_toilets
+    def __init__(self, id, name, capacity, num_bars=0, num_toilets=0):
+        self.id = id
+        self.name = name
+        self.capacity = capacity
+        self.num_bars = num_bars
+        self.num_toilets = num_toilets
 
     def __repr__(self):
-        return f"<Location {self.name}, capacity={self.capacity}, Bars={self.num_bars}, Toilets={self.num_toilets}>"
-    
+        return (f"<Location {self.name}, capacity={self.capacity}, "
+                f"Bars={self.num_bars}, Toilets={self.num_toilets}>")
 
-    @validates("num_bars", "num_toilets")
-    def validate_numbers(self, key, value):
-        if not isinstance(value, int) or value < 0:
-            raise ValueError(f"{key} must be an integer")
-        return value
+    @property
+    def name(self):
+        return self._name
 
-
-    @validates("capacity")
-    def validate_capacity(self, key, value):
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError("Capacity must be a positive integer")
-        return value
-
-    @validates
-    def validate_name(self, ket, value):
+    @name.setter
+    def name(self, value):
         if not isinstance(value, str) or not value.strip():
-            raise ValueError("Please enter a location name")
-        return value
+            raise ValueError("Please enter a valid location name")
+        self._name = value.strip()
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Capacity must be a positive number")
+        self._capacity = value
+
+    @property
+    def num_bars(self):
+        return self._num_bars
+
+    @num_bars.setter
+    def num_bars(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise ValueError("Number of bars must be more than 0")
+        self._num_bars = value
+
+    @property
+    def num_toilets(self):
+        return self._num_toilets
+
+    @num_toilets.setter
+    def num_toilets(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise ValueError("Number of toilets must be a number more than 0")
+        self._num_toilets = value
 
     @classmethod
-    def create(cls, session, name, capacity, num_bars=0, num_toilets=0):
-        location = cls(
-            name=name,
-            capacity=capacity,
-            num_bars=num_bars,
-            num_toilets=num_toilets
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS locations (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            capacity INTEGER NOT NULL,
+            num_bars INTEGER DEFAULT 0,
+            num_toilets INTEGER DEFAULT 0)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
 
-        )
-        session.add(location)
-        session.commit()
+    @classmethod
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, capacity, num_bars, num_toilets)
+            VALUES (?, ?, ?, ?)
+         """
+
+        CURSOR.execute(sql, (self.name, self.capacity,
+                       self.num_bars, self.num_toilets))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls, name, capacity, num_bars, num_toilets):
+        location = cls(name, capacity, num_bars, num_toilets)
+        location.save()
+
+    def update(self):
+        sql = """
+            UPDATE locations
+            SET name = ?, capacity = ?, num_bars = ?, num_toilets = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.capacity,
+                       self.num_bars, self.num_toilets))
+        CONN.commit()
+
+    def delete(self):
+        sql = """
+            DELETE FROM locations
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+
+        del type(self).all[self.id]
+        self.id = None
+
+    @classmethod
+    def instance_from_db(cls, row):
+        location = cls.all.get(row[0])
+        if location:
+            location.name = row[1]
+            location.capacity = row[2]
+            location.num_bars = row[3]
+            location.num_toilets = row[4]
+        else:
+            location = cls(row[1], row[2], row[3], row[4])
+            location.id = row[0]
+            cls.all[location.id] = location
         return location
-    
+        
+
     @classmethod
-    def get_all(cls, session):
-        return session.query(cls).all()
-    
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM locations
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
+
     @classmethod
-    def find_by_id(cls, session, location_id):
-        return session.get(cls, location_id)
+    def find_by_name(cls, name):
+        sql = """"
+        SELECT *
+        FROM locations
+        WHERE name is ?
+        """
+        
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
-    def delete(self, session):
-        session.delete(self)
-        session.commit()
 
-    # creates the location table
-Base.metadata.create_all(engine) 
-
-
-
-    
