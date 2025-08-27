9f28f5444392db0c3dfdc273ee3a107cf9e3a522
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-28 01:28:17 +0300

    Adds menu changes

--- lib/event_wizard_db.py
+++ lib/event_wizard_db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///event_wizard.db')
+engine = create_engine('sqlite:///event_wizard.db', echo=True)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
