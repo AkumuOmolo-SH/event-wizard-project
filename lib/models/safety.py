c096105ac056f83ce7fdb50ce7b6463100a736e7
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-28 03:37:12 +0300

    Changes class relationship to event

--- lib/models/safety.py
+++ lib/models/safety.py
@@ -2,16 +2,19 @@ from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship, validates
 from lib.event_wizard_db import Base
 
+
 class Safety(Base):
     __tablename__ = "safety"
 
     id = Column(Integer, primary_key=True)
-    ambulances = Column(Integer, default=0)       
-    nurses = Column(Integer, default=0)           
-    security_staff = Column(Integer, default=0)   
-    event_id = Column(Integer, ForeignKey("events.id"), nullable=False, unique=True)
+    ambulances = Column(Integer, default=0)
+    nurses = Column(Integer, default=0)
+    security_staff = Column(Integer, default=0)
+    event_id = Column(Integer, ForeignKey("events.id"),
+                      nullable=False, unique=True)
 
     event = relationship("Event", back_populates="safety")
+    # location = relationship("Location", back_populates="safety")
 
     def __repr__(self):
         return (
@@ -24,3 +27,20 @@ class Safety(Base):
         if not isinstance(value, int) or value < 0:
             raise ValueError(f"{key} must be a positive number")
         return value
+
+    @staticmethod
+    def suggest_staff(expected_attendees):
+        expected_attendees = int(expected_attendees)
+
+        security_staff = max(15, expected_attendees // 100)
+        nurses = max(6, expected_attendees // 500)
+        ambulances = max(2, expected_attendees // 500)
+
+        return {
+            "For" :expected_attendees, 
+            "security_staff": security_staff, 
+            "nurses": nurses, 
+            "ambulances": ambulances
+            }
+    
+
